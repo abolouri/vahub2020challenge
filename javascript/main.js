b612664afa32ddd4ba02e748ac033bd16116d49e
Eva Jelinkova <jelin.eva@gmail.com>
2020-05-30 23:07:51 +0100

    reload function after adding task

--- javascript/main.js
+++ javascript/main.js
@@ -25,32 +25,27 @@ buttonAddTask.setAttribute('id', 'addtaskbtn');
 buttonAddTask.textContent = `+`;
 buttonAddTask.setAttribute('onClick', 'addTask();');
 
-    function addTask() {
-      var newTask = document.querySelector("input");
-      // var taskTitle = document.createTextNode(newTask.value);
-      var taskTitle = newTask.value;
-
-      var saveTask = new XMLHttpRequest();
-      saveTask.open('POST', 'https://va2020concept.cf/api/v1/tasks', true);
-      saveTask.setRequestHeader("Content-Type", "application/json");
-      console.log(taskTitle);
-      var data = JSON.stringify({"title": taskTitle});
-      saveTask.send(data);
-        
-      }
+function addTask() {
+  var newTask = document.querySelector("input");
+  var payload = 'title=' + newTask.value;
+  var saveTask = new XMLHttpRequest();
+  saveTask.open('POST', 'https://va2020concept.cf/api/v1/tasks', true);
+  saveTask.setRequestHeader("Content-Type", "application/json");
+  console.log(payload);
+  saveTask.send(payload);
+  // reload page after adding a new task to show all available tasks
+  saveTask.onload = function () {
+    window.location.reload(true);
+  }
+}
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
-
-
 
 app.appendChild(inputTask);
 app.appendChild(buttonAddTask);
 app.appendChild(container);
 // app.appendChild(logo);
-
-
-
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://va2020concept.cf/api/v1/tasks', true);
