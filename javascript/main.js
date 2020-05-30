b51947a1d5d16e40e75b2b394e62822e408874ff
Eva Jelinkova <jelin.eva@gmail.com>
2020-05-30 19:23:17 +0100

    added date, add button

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,48 @@
+
+
 const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate(); 
+
+document.getElementById("currentDate").innerHTML = date;
+
+
+const inputTask = document.createElement('input');
+inputTask.setAttribute("type", "text");
+inputTask.setAttribute("placeholder", "task name");
+
+const buttonAddTask = document.createElement('button');
+buttonAddTask.setAttribute('class', 'addButton');
+buttonAddTask.setAttribute('id', 'addtaskbtn');
+buttonAddTask.textContent = `add`;
+buttonAddTask.setAttribute('onClick', 'addTask();');
+
+    function addTask() {
+      var newTask = document.querySelector("input");
+      // var taskTitle = document.createTextNode(newTask.value);
+      var taskTitle = newTask.value;
+
+      var saveTask = new XMLHttpRequest();
+      saveTask.open('POST', 'https://va2020concept.cf/api/v1/tasks', true);
+      saveTask.setRequestHeader("Content-Type", "application/json");
+      console.log(taskTitle);
+      var data = JSON.stringify({"title": taskTitle});
+      saveTask.send(data);
+        
+      }
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-app.appendChild(logo);
+
+
+app.appendChild(inputTask);
+app.appendChild(buttonAddTask);
 app.appendChild(container);
+
+
+
 
 var request = new XMLHttpRequest();
 request.open('GET', 'https://va2020concept.cf/api/v1/tasks', true);
@@ -38,3 +73,32 @@ request.onload = function () {
 }
 
 request.send();
+
+
+
+
+// -------------------------------add item to list------------
+
+// function addItem() {
+//   var newListItem = document.createElement("li");
+//   newListItem.className = "listitem";
+
+//   var input = document.querySelector("input");
+
+//   var text = document.createTextNode(input.value);
+//   newListItem.appendChild(text);
+
+//   var list = document.querySelector(".list");
+//   list.appendChild(newListItem);
+// }
+
+
+// <main class="container">
+//     <input class="submission-line__input" type="text" placeholder="Enter new item here...">
+//     <button onclick="addItem();" class="submission-line__btn">Add</button>
+//     <ul class="list">
+//       <li class="listitem">First</li>
+//       <li class="listitem">Second</li>
+//       <li class="listitem">Third</li>
+//     </ul>
+//   </main>
